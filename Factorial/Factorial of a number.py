14de8d6b638c43394bc1bfe3650705cac5be00eb
paiabhay <abhay.u.pai@aol.com>
2017-12-20 21:07:23 +0100

    Factorial of a number

---
+++ Factorial/Factorial of a number.py
@@ -0,0 +1,19 @@
+#Find factorial of a given number
+import math
+
+#take the  umber as input from user
+print("Enter a number for finding its factorial: ")
+value = int(input())
+
+#calulate factorial
+def fact(k):
+    factorial = 1       #Initialize
+    if k > 0:
+        for i in range(1, k+1):
+            factorial = factorial * i
+        print("factorial of ",value," is: ",factorial)
+    elif k == 0:
+        print("factorial of ", value, " is: ", factorial)
+    else:
+        print("Cannot calculate factorial of a negative number")
+fact(value)
