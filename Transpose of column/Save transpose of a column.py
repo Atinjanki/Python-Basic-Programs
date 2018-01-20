ac89b43016d50e0e9775e24e4ef28435bab4703b
paiabhay <abhay.u.pai@aol.com>
2018-01-20 22:10:19 +0100

    Transpose of column Also save the same in a file

---
+++ Transpose of column/Save transpose of a column.py
@@ -0,0 +1,21 @@
+import csv
+from itertools import zip_longest
+
+#Create rows
+row_one = [1,2,3,4,5,6,7,8,9,10]
+row_two = [11,12,13,14,15,16,17,18,19,20]
+combined_list = []
+
+combined_list.append(row_one)
+combined_list.append(row_two)
+print(combined_list)
+
+#zip longest is used for transpose of a column
+data = zip_longest(*combined_list)
+
+#Save in a file the values
+with open('new_file.csv', 'w', newline='') as f:
+    wr = csv.writer(f)
+    wr.writerows(data)
+    print('\nFile written successfully')
+f.close()
