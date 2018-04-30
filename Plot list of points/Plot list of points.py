d0ee3f35ed1ea4b5bd69c6a3e6a96b8fe24d6441
paiabhay <abhay.u.pai@aol.com>
2018-04-30 15:57:06 +0200

    Plot graph from list of points

---
+++ Plot list of points/Plot list of points.py
@@ -0,0 +1,28 @@
+#Take as input points from user and plot those points on graph
+
+from random import randint
+import numpy as np
+import matplotlib.pyplot as plt
+
+coordinates=[]
+temp=[]
+points = int(input("Enter No Of Coordinates: "))
+
+#Take input the co-ordinates of points and store it in array
+for i in range(0,points):
+    print('Enter X co-ordinate of point', i+1)
+    x_coordinate = int(input())
+    print('Enter Y co-ordinate of point', i+1)
+    y_coordinate = int(input())
+    temp=[]
+    temp.append(x_coordinate)
+    temp.append(y_coordinate)
+    coordinates.append(temp)
+
+print('All co-ordinates are: ',coordinates)
+
+#Ploting on graph
+splitting_coordinates = np.array(coordinates)
+x,y = splitting_coordinates.T
+plt.scatter(x,y)
+plt.show()
