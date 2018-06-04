fbf368a38419194e763531e96884d8341d3ec090
Abhay Pai <abhay.u.pai@aol.com>
2018-06-04 10:22:41 +0900

    Update Save transpose of a column.py

--- Transpose of column/Save transpose of a column.py
+++ Transpose of column/Save transpose of a column.py
@@ -1,6 +1,7 @@
-import csv
+import csv 
 from itertools import zip_longest
 
+#File also available on Bitbucket
 #Create rows
 row_one = [1,2,3,4,5,6,7,8,9,10]
 row_two = [11,12,13,14,15,16,17,18,19,20]
